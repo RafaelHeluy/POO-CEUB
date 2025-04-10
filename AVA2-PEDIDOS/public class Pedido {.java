bf66e089bbaa2dc8da3f1f90eae80db044ddd128
Rafael de Almeida Heluy <rafael.heluy@sempreceub.com>
2025-04-10 10:47:12 -0300

    AVA2-2-POO-QUI-Rafael de Almeida Heluy Araujo
    AVA2-2-POO-QUI-Rafael de Almeida Heluy Araujo

---
+++ AVA2-PEDIDOS/public class Pedido {.java
@@ -0,0 +1,24 @@
+public class Pedido 
+{
+    private String identificadorPedido;
+    private Date dataHora;
+    private String statusPedido;
+    private int valorTotal;
+public class Item {
+    private Int CodigoItem;
+    private String DescriçãoProduto;
+    private Int quantidadePedida;
+    private Float preçoUnitario;
+}
+public class Cliente {
+    private String nomeCliente
+    private String cpfCliente
+    private String telefoneCliente
+    private String endereçoCliente
+}
+public class Vendedor {
+    private String nomeVendedor;
+    private String matriculaVendedor;
+}
+private static float calcular valorTotal()
+}
